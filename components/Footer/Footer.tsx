f353a8533b4e6a522f0b4e03e811754fde65542d
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-10-04 13:01:01 +0600

    footer updated

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,11 +8,11 @@ const Footer = () => {
   return (
     <>
       <section className="footer">
-        <div className="pt-[100px] w-[95%] mx-auto">
+        <div className="pt-[70px] w-[95%] mx-auto">
           <Image src={Child} alt="child" />
         </div>
-        <div className="bg-white text-black -mt-[5px] w-[89.7%] ml-[80px]">
-          <div className="grid grid-cols-12 place-items-center gap-x-[40px]">
+        <div className="bg-white text-black -mt-[5px] w-[89.7%] ml-[80px] px-[40px]">
+          <div className="grid grid-cols-12 place-items-center gap-x-[60px] border-b-[4px] border-[#F8F1AC] pb-[20px]">
             <div className="col-span-4">
               <h2 className="brand__logo laptop__screen:flex items-center flex-grow laptop__screen:font-[400]">
                 <span className="laptop__screen:font-greatFredricka laptop__screen:text-[38px] laptop__screen:leading-[69px] laptop__screen:text-[#4A1E9E]">
@@ -23,27 +23,30 @@ const Footer = () => {
                 </span>
               </h2>
               <div>
-                <h3>About Us</h3>
-                <p>
+                <h3 className="text-[20px] font-[600] font-robotoSerif text-[#8A2BE2]">
+                  About Us
+                </h3>
+                <p className="text-[14px] font-robotoSerif font-[400] w-[250px]">
                   We provide platform for the students to explore STEM( Science,
                   Tech, Engineering, Mathematics) so to make them learn while
                   having fun and provide a competitive environment.
                 </p>
               </div>
-              <div>
-                <h3>News Letter</h3>
-                <input
-                  type="text"
-                  className="border-[2px] border-[#000]"
-                />{" "}
-                <button>Send</button>
+              <div className="my-[10px]">
+                <h3 className="text-[20px] font-[600] font-robotoSerif text-[#8A2BE2]">
+                  News Letter
+                </h3>
+                <div>
+                  <input placeholder="Email" type="text" className="border-[2px] border-[#8A2BE2] placeholder:text-[#8A2BE2] text-[16px] font-[600] font-robotoSerif  px-[10px] py-[3px] rounded-[8px]"  />
+                  <button className="bg-[#8A2BE2] text-[#fff] px-[15px] py-[4px] rounded-[8px] ml-[8px]">Send</button>
+                </div>
               </div>
             </div>
             <div className="col-span-4">
-              <div className="flex justify-around">
+              <div className="flex justify-around gap-x-[80px]">
                 <div>
-                  <h3>Navigations</h3>
-                  <ul>
+                  <h3 className="text-[20px] font-[600] font-robotoSerif text-[#8A2BE2]">Navigations</h3>
+                  <ul className="font-robotoSerif">
                     <li>
                       <a href="#">Home</a>
                     </li>
@@ -62,8 +65,8 @@ const Footer = () => {
                   </ul>
                 </div>
                 <div>
-                  <h3>Navigations</h3>
-                  <ul>
+                  <h3 className="text-[20px] font-[600] font-robotoSerif text-[#8A2BE2]">Navigations</h3>
+                  <ul className="font-robotoSerif">
                     <li>
                       <a href="#">News Letter</a>
                     </li>
@@ -84,24 +87,27 @@ const Footer = () => {
               </div>
             </div>
             <div className="col-span-4">
-              <h3>Contact</h3>
-              <div className="flex items-center">
-                <HiLocationMarker />
-                <p>
+              <h3 className="text-[20px] font-[600] font-robotoSerif text-[#8A2BE2]">Contact</h3>
+              <div className="flex items-center text-[#565656]">
+                <HiLocationMarker size={20}/>
+                <p className="w-[200px] font-robotoSerif text-[14px] px-[10px]">
                   Ward no.5,Bhinjal pada District-Bargarh State-Odisha Pin
                   Code-768028
                 </p>
               </div>
-              <div className="flex items-center">
-                <BsTelephoneFill />
-                <p>7978111219,7205181669</p>
+              <div className="flex items-center my-[8px] text-[#565656] font-robotoSerif text-[14px]">
+                <BsTelephoneFill size={15}/>
+                <p className="w-[200px] font-robotoSerif text-[14px] px-[10px]">7978111219,7205181669</p>
               </div>
-              <div className="flex items-center">
-                <AiFillFacebook />
-                <AiFillInstagram />
-                <AiFillMail />
+              <div className="flex items-center gap-[20px] my-[4px]">
+                <AiFillInstagram className="bg-[#8A2BE2] rounded-[30px] p-[2px]" fill="#fff" size={22}/>
+                <AiFillMail className="bg-[#8A2BE2] rounded-[30px] p-[2px]" fill="#fff" size={22}/>
+                <AiFillFacebook className="bg-[#8A2BE2] rounded-[30px] p-[2px]" fill="#fff" size={22}/>
               </div>
             </div>
+          </div>
+          <div className="text-center text-[14px] font-robotoSerif font-[400] py-[12px]">
+            <p>Copyrights©2022 All Rights Reserved</p>
           </div>
         </div>
       </section>
